dd9132c84ef78c57b0e2e4e60a1b1b3ce480f2e0
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-08-06 19:02:44 +0000

    server 2 setup

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main(){}
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"revproxy/server2"
+)
+
+func main() {
+	server2Router := mux.NewRouter()
+	fmt.Println("Starting server at port 8000")
+	server2.SetUpHandlers(server2Router)
+	log.Fatal(http.ListenAndServe(":8000", server2Router))
+}
